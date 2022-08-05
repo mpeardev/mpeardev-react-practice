c646c4110911290c8d53716e6274fae1e371757d
mirkoperamas <1421133@utp.edu.pe>
2022-08-04 22:24:34 -0500

    2 files modified

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export * from './Main';
+export * from './Header';
 export * from './exercises/ActressesGallery';
 export * from './exercises/QuotesList';
 export * from './exercises/FormTypes/FormTypes';
